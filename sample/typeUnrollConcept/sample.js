894df2bdb1bd172a0405ce06e1a822f36eab78ee
Artem Bohush <art.bohush@gmail.com>
2020-06-09 11:58:44 +0300

    unroll type edited, typo fixed

--- sample/typeUnrollConcept/sample.js
+++ sample/typeUnrollConcept/sample.js
@@ -1,11 +1,22 @@
 const _ = require( 'wTools' );
 
-var unroll1 = _.unrollMake( [ 1, 1, 3 ] );
-var unroll2 = _.unrollMake( [ 4, 5, 6 ] );
-var unroll3 = _.unrollMake( [ 7, 8, 9 ] );
-unroll1[ 3 ] = unroll2;
-unroll2[ 3 ] = unroll3;
-console.log( unroll1 );
+var unroll1 = _.unrollMake( [ 1, 2 ] );
+var unroll2 = _.unrollMake( [ 3, 4 ] );
+var unroll3 = _.unrollMake( [ 5, 6 ] );
 
-var result = _.unrollFrom( [ 'a', 'b', unroll1 ] );
+unroll1[ 2 ] = unroll2;
+unroll2[ 2 ] = unroll3;
+
+console.log( unroll1 ); // [ 1, 2, [ 3, 4 [ 5, 6 ] ] ]
+console.log( unroll2 ); // [ 3, 4, [ 5, 6 ] ]
+console.log( unroll3 ); // [ 5, 6  ]
+
+var result = _.unrollNormalize( [ unroll1, unroll2, unroll3 ] );
 console.log( result );
+/*
+[
+  1, 2, [ 3, 4, [ 5, 6 ] ],
+  3, 4, [ 5, 6 ],
+  5, 6
+]
+*/
